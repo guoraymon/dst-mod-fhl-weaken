7519a46b61c8a71ca0412e1f2354137bce1508b7
raymon <guoraymon@gmail.com>
2020-05-21 22:36:53 +0800

    三维调整

--- scripts/prefabs/fhl.lua
+++ scripts/prefabs/fhl.lua
@@ -69,26 +69,27 @@ end
 
 --升级机制
 local function applyupgrades(inst)
-
 	local max_upgrades = 30
 	local upgrades = math.min(inst.level, max_upgrades)
+
+	upgrades = 0
 	
 	local hunger_percent = inst.components.hunger:GetPercent()
 	local health_percent = inst.components.health:GetPercent()
 	local sanity_percent = inst.components.sanity:GetPercent()
 
-	inst.components.hunger.max = math.ceil (150 + upgrades * 5) --300
-	inst.components.health.maxhealth = math.ceil (150 + upgrades * 5) --300
-	inst.components.sanity.max = math.ceil (200 + upgrades * 3) --290
+	inst.components.hunger.max = math.ceil (120 + upgrades * 2) --150
+	inst.components.sanity.max = math.ceil (200 + upgrades * 2) --250
+	inst.components.health.maxhealth = math.ceil (120 + upgrades * 2) --150
 	
 	inst.components.locomotor.walkspeed = math.ceil (6 + upgrades / 10) --9
 	inst.components.locomotor.runspeed = math.ceil (8 + upgrades / 10) --11
 	
-	inst.components.talker:Say("QWQ Level up: ".. (inst.level).. "\n你还有".. (inst.jnd).. "点技能点!\n加点帮助请按R,当前状态请按T!".. "\nyou have ".. (inst.jnd).. " skill points!\nClick R for help,Click T for State!")
+	-- inst.components.talker:Say("QWQ Level up: ".. (inst.level).. "\n你还有".. (inst.jnd).. "点技能点!\n加点帮助请按R,当前状态请按T!".. "\nyou have ".. (inst.jnd).. " skill points!\nClick R for help,Click T for State!")
 	
-	if inst.level >29 then
-		inst.components.talker:Say("W.W Level Max!\n你还有".. (inst.jnd).. "点技能点!\n加点帮助请按R,当前状态请按T!".. "\nyou have ".. (inst.jnd).. " skill points!\nClick R for help,Click T for State!")
-	end
+	-- if inst.level >29 then
+	-- 	inst.components.talker:Say("W.W Level Max!\n你还有".. (inst.jnd).. "点技能点!\n加点帮助请按R,当前状态请按T!".. "\nyou have ".. (inst.jnd).. " skill points!\nClick R for help,Click T for State!")
+	-- end
 
 	inst.components.hunger:SetPercent(hunger_percent)
 	inst.components.health:SetPercent(health_percent)
@@ -198,7 +199,7 @@ local master_postinit = function(inst)
 	inst.level = 0
 	inst.jnd = 0
 	inst.je = 0
-	inst.components.eater:SetOnEatFn(oneat)
+	-- inst.components.eater:SetOnEatFn(oneat)
 	applyupgrades(inst)
 	
 	inst:AddComponent("reader")	
