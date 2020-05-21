f25ad64af809baaba9043296be16c2d12ebcd1b3
raymon <guoraymon@gmail.com>
2020-05-21 22:51:43 +0800

    背包专属，无护甲

--- scripts/prefabs/fhl_bb.lua
+++ scripts/prefabs/fhl_bb.lua
@@ -6,28 +6,28 @@ local assets =
 	Asset("ATLAS", "images/inventoryimages/fhl_bb.xml")
 }
 
-local function AcceptTest(inst, item)
-	if item.prefab == "ancient_soul" and inst.components.armor:GetPercent() < 1 then
-		return true
-	elseif inst.components.armor:GetPercent() == 1 then
-		local owner = item.components.inventoryitem:GetGrandOwner()
-		owner.components.talker:Say("耐久已满!\nDurability is full!")
-	else
-		local owner = item.components.inventoryitem:GetGrandOwner()
-		owner.components.talker:Say("qwq这个无法用来修复背包哦!\nThis can't be used to repairing the backpack!")
-	end
-end
+-- local function AcceptTest(inst, item)
+-- 	if item.prefab == "ancient_soul" and inst.components.armor:GetPercent() < 1 then
+-- 		return true
+-- 	elseif inst.components.armor:GetPercent() == 1 then
+-- 		local owner = item.components.inventoryitem:GetGrandOwner()
+-- 		owner.components.talker:Say("耐久已满!\nDurability is full!")
+-- 	else
+-- 		local owner = item.components.inventoryitem:GetGrandOwner()
+-- 		owner.components.talker:Say("qwq这个无法用来修复背包哦!\nThis can't be used to repairing the backpack!")
+-- 	end
+-- end
 
-local function OnGetItemFromPlayer(inst, giver, item)
-	if item.prefab == "ancient_soul" and inst.components.armor:GetPercent() < 1 then
-		giver.components.inventory:ConsumeByName("ancient_soul", 1)
-		local backpack_repaired = TUNING.ARMORMARBLE*1.2
-		inst.components.armor.condition = inst.components.armor.condition + backpack_repaired
-		if inst.components.armor:GetPercent() > 1 then
-			inst.components.armor:SetCondition(TUNING.ARMORMARBLE*3)
-		end
-	end
-end
+-- local function OnGetItemFromPlayer(inst, giver, item)
+-- 	if item.prefab == "ancient_soul" and inst.components.armor:GetPercent() < 1 then
+-- 		giver.components.inventory:ConsumeByName("ancient_soul", 1)
+-- 		local backpack_repaired = TUNING.ARMORMARBLE*1.2
+-- 		inst.components.armor.condition = inst.components.armor.condition + backpack_repaired
+-- 		if inst.components.armor:GetPercent() > 1 then
+-- 			inst.components.armor:SetCondition(TUNING.ARMORMARBLE*3)
+-- 		end
+-- 	end
+-- end
 
 local function onbreak(owner,data)
 	local armor = data ~= nil and data.armor or nil
@@ -39,10 +39,25 @@ local function onbreak(owner,data)
 end
 
 local function onequip(inst, owner)
-    owner.AnimState:OverrideSymbol("swap_body", "swap_fhl_bb", "symbol_15220700")
-    owner.AnimState:OverrideSymbol("swap_body", "swap_fhl_bb", "symbol_b6d8e12e")
-    inst.components.container:Open(owner)
-	inst:ListenForEvent("armorbroke",onbreak,owner)
+	-- 风幻龙专属
+	if owner.prefab == "fhl"  then
+		owner.AnimState:OverrideSymbol("swap_body", "swap_fhl_bb", "symbol_15220700")
+		owner.AnimState:OverrideSymbol("swap_body", "swap_fhl_bb", "symbol_b6d8e12e")
+		inst.components.container:Open(owner)
+		inst:ListenForEvent("armorbroke",onbreak,owner)
+	else
+		owner:DoTaskInTime(0, function()
+			local inventory = owner.components.inventory 
+			if inventory then
+				inventory:DropItem(inst)
+			end
+
+			local talker = owner.components.talker 
+			if talker then
+				talker:Say(STRINGS.FHL_TEDING)
+			end
+		end)
+	end
 end
 
 local function onunequip(inst, owner)
@@ -99,16 +114,16 @@ local function fn()
 	inst.components.inventoryitem.foleysound = "dontstarve/movement/foley/marblearmour"
     inst.components.inventoryitem.atlasname = "images/inventoryimages/fhl_bb.xml"
 	
-	inst:AddComponent("waterproofer")
-    inst.components.waterproofer:SetEffectiveness(TUNING.WATERPROOFNESS_HUGE)
+	-- inst:AddComponent("waterproofer")
+    -- inst.components.waterproofer:SetEffectiveness(TUNING.WATERPROOFNESS_HUGE)
 	
-	if TUNING.FHL_HJOPEN then
-		inst:AddComponent("armor")
-		inst.components.armor:InitCondition(TUNING.ARMORMARBLE*3, 0.8)
-		inst:AddComponent("trader")
-		inst.components.trader:SetAcceptTest(AcceptTest)
-		inst.components.trader.onaccept = OnGetItemFromPlayer
-	end
+	-- if TUNING.FHL_HJOPEN then
+	-- 	inst:AddComponent("armor")
+	-- 	inst.components.armor:InitCondition(TUNING.ARMORMARBLE*3, 0.8)
+	-- 	inst:AddComponent("trader")
+	-- 	inst.components.trader:SetAcceptTest(AcceptTest)
+	-- 	inst.components.trader.onaccept = OnGetItemFromPlayer
+	-- end
 	
     inst:AddComponent("equippable")
     inst.components.equippable.equipslot = EQUIPSLOTS.BACK or EQUIPSLOTS.BODY
